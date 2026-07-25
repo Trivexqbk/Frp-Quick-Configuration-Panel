213bfd9469d1e76710893ed910da606cf378e0d3
medtransporter-design <medtransporter@gmail.com>
2026-07-25 19:01:16 +0300

    rush

---
+++ installer/setup/resource.h
@@ -0,0 +1,16 @@
+#ifndef _RESOURCE_H
+#define _RESOURCE_H
+
+#define IDI_ICON        10
+#define IDR_MSI         11
+#define IDD_LANG_DIALOG 100
+#define IDC_LANG_COMBO  1000
+#define IDC_STATIC      -1
+
+#define IDS_TITLE       200
+#define IDS_MANAGEMENT  201
+#define IDS_OPERATION   202
+#define IDS_REINSTALL   203
+#define IDS_UNINSTALL   204
+
+#endif
